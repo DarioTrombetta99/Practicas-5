f97961e36f7ee36f07ca80742146358eb2de577a
Dario Trombetta <75943701+DarioTrombetta99@users.noreply.github.com>
2021-10-21 22:31:02 +0100

    Add files via upload

--- Practicas5/src/main/java/com/mycompany/practicas5/Lienzo.java
+++ Practicas5/src/main/java/com/mycompany/practicas5/Lienzo.java
@@ -1,7 +1,5 @@
 package com.mycompany.practicas5;
 
-
-
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
@@ -12,29 +10,42 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.logging.Level;
 import java.io.IOException;
+
 /**
-*
-* @author CiaMb
-*/
+ *
+ * @author CiaMb
+ */
 public class Lienzo extends JPanel {
 
-private BufferedImage images = null;
+    private BufferedImage images = null;
+    
+    private BufferedImage image2 = null;
 
-public Lienzo(){
-try{
-images = ImageIO.read(new URL("https://www.lucelight.it/fotoprog.php/thumb/lighting_jetty_on_lake_lesina-99581.jpg"));
-this.setPreferredSize(new Dimension(images.getWidth(),images.getWidth()));
-}catch (MalformedURLException ex){
-Logger.getLogger(Lienzo.class.getName()).log(Level.SEVERE, null, ex);
-}catch (IOException ex){
-Logger.getLogger(Lienzo.class.getName()).log(Level.SEVERE, null, ex);
-}
-}
+    public Lienzo() {
+        try {
+            images = ImageIO.read(new URL("https://www.lucelight.it/fotoprog.php/thumb/lighting_jetty_on_lake_lesina-99581.jpg"));
+            image2 = ImageIO.read(new URL("https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/50px_x_50px_coloured_logo_on_white_background.svg/68px-50px_x_50px_coloured_logo_on_white_background.svg.png"));
+            System.out.println("Logo Width: logo " + image2.getWidth() + " height: " + image2.getHeight());
+            this.setPreferredSize(new Dimension(images.getWidth(), images.getHeight()));
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(Lienzo.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Lienzo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public BufferedImage getImage() {
+        return images;
+    }
+    
+    public BufferedImage getImage2() {
+        return image2;
+    }
 
-@Override
-public void paintComponent (Graphics g){
-super.paintComponent(g);
-g.drawImage(images, 0, 0, null);
-}
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(images, 0, 0, null);
+    }
 
 }
--- Practicas5/src/main/java/com/mycompany/practicas5/NewJFrame.java
+++ Practicas5/src/main/java/com/mycompany/practicas5/NewJFrame.java
@@ -5,7 +5,16 @@
  */
 package com.mycompany.practicas5;
 
+import java.awt.Color;
+import java.awt.List;
+import java.awt.Point;
 import java.awt.event.ItemEvent;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Logger;
 
 /**
  *
@@ -13,11 +22,37 @@ import java.awt.event.ItemEvent;
  */
 public class NewJFrame extends javax.swing.JFrame {
 
+    private static final Logger logger = Logger.getLogger("NewJFrame");
+
+    private static final Color red = new Color(255, 0, 0);
+
+    private static final Color green = new Color(0, 255, 0);
+
+    private static final Color blue = new Color(0, 0, 255);
+
+    private boolean isRedCheked = true;
+
+    private boolean isGreenCheked = true;
+
+    private boolean isBlueCheked = true;
+    
+    private PictureLocation location = PictureLocation.LEFT_UP;
+
+    private final Map<Point, Color> colors = new HashMap<>();
+    
+    private final Map<Point, Integer> pointsToDeleteLogo = new HashMap<>();
+    
+
     /**
      * Creates new form NewJFrame
      */
     public NewJFrame() {
         initComponents();
+        initializeColors();
+        jCheckBox1.setSelected(true);
+        jCheckBox2.setSelected(true);
+        jCheckBox3.setSelected(true);
+        jCheckBox4.setSelected(true);
     }
 
     /**
@@ -45,7 +80,7 @@ public class NewJFrame extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(244, 244, 244));
-        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Set colors of the image", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 0, 11), new java.awt.Color(51, 51, 51))); // NOI18N
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Set colors of the image", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Dialog", 0, 12), new java.awt.Color(51, 51, 51))); // NOI18N
         jPanel1.setToolTipText("Setting colors");
         jPanel1.setName("Setting colors"); // NOI18N
 
@@ -189,11 +224,11 @@ public class NewJFrame extends javax.swing.JFrame {
         lienzo1.setLayout(lienzo1Layout);
         lienzo1Layout.setHorizontalGroup(
             lienzo1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 926, Short.MAX_VALUE)
+            .addGap(0, 900, Short.MAX_VALUE)
         );
         lienzo1Layout.setVerticalGroup(
             lienzo1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 620, Short.MAX_VALUE)
+            .addGap(0, 900, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -206,7 +241,7 @@ public class NewJFrame extends javax.swing.JFrame {
                         .addContainerGap()
                         .addComponent(lienzo1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(119, 119, 119)
+                        .addGap(128, 128, 128)
                         .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(128, 128, 128)
                         .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
@@ -229,6 +264,8 @@ public class NewJFrame extends javax.swing.JFrame {
 
     private void jCheckBox2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox2ActionPerformed
         // TODO add your handling code here:
+
+
     }//GEN-LAST:event_jCheckBox2ActionPerformed
 
     private void jCheckBox3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox3ActionPerformed
@@ -238,11 +275,14 @@ public class NewJFrame extends javax.swing.JFrame {
     private void jRadioButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton4ActionPerformed
         // TODO add your handling code here:
         System.err.println("Logo posizionato in basso a sinistra");
+        location = PictureLocation.LEFT_DOWN;
+        setLocation();
+        
     }//GEN-LAST:event_jRadioButton4ActionPerformed
 
     private void jCheckBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox1ActionPerformed
         // TODO add your handling code here:
-       
+
     }//GEN-LAST:event_jCheckBox1ActionPerformed
 
     private void jCheckBox4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox4ActionPerformed
@@ -254,12 +294,33 @@ public class NewJFrame extends javax.swing.JFrame {
         System.out.println("Opzione rosso cambiata");
         int rosso = evt.getStateChange();
         System.out.println("Stato: " + rosso);
-        if(rosso == ItemEvent.SELECTED){
+        if (rosso == ItemEvent.SELECTED) {
             System.out.println("Colore rosso selezionato!");
+            isRedCheked = true;
+            if (isRedCheked && isBlueCheked && isGreenCheked) {
+                jCheckBox1.setEnabled(false);
+                jCheckBox1.setSelected(true);
+            }
+            jCheckBox3.setEnabled(true);
+            jCheckBox4.setEnabled(true);
+
         }
-        if(rosso == ItemEvent.DESELECTED){
+        if (rosso == ItemEvent.DESELECTED) {
+            jCheckBox1.setEnabled(true);
+            jCheckBox1.setSelected(false);
             System.out.println("Colore rosso deselezionato");
+            if (isGreenCheked || isBlueCheked) {
+                isRedCheked = false;
+            }
+            if (isGreenCheked && !isBlueCheked) {
+                jCheckBox3.setEnabled(false);
+            } else if (!isGreenCheked && isBlueCheked) {
+                jCheckBox4.setEnabled(false);
+            }
+
         }
+        setColors();
+        setLocation();
     }//GEN-LAST:event_jCheckBox2ItemStateChanged
 
     private void jCheckBox3ItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_jCheckBox3ItemStateChanged
@@ -267,12 +328,31 @@ public class NewJFrame extends javax.swing.JFrame {
         System.out.println("Opzione verde cambiata");
         int verde = evt.getStateChange();
         System.out.println("Stato: " + verde);
-        if(verde == ItemEvent.SELECTED){
+        if (verde == ItemEvent.SELECTED) {
             System.out.println("Colore verde selezionato!");
+            isGreenCheked = true;
+            if (isRedCheked && isBlueCheked && isGreenCheked) {
+                jCheckBox1.setEnabled(false);
+                jCheckBox1.setSelected(true);
+            }
+            jCheckBox2.setEnabled(true);
+            jCheckBox4.setEnabled(true);
         }
-        if(verde == ItemEvent.DESELECTED){
+        if (verde == ItemEvent.DESELECTED) {
+            jCheckBox1.setEnabled(true);
+            jCheckBox1.setSelected(false);
             System.out.println("Colore verde deselezionato");
+            if (isRedCheked || isBlueCheked) {
+                isGreenCheked = false;
+            }
+            if (isRedCheked && !isBlueCheked) {
+                jCheckBox2.setEnabled(false);
+            } else if (!isRedCheked && isBlueCheked) {
+                jCheckBox4.setEnabled(false);
+            }
         }
+        setColors();
+        setLocation();
     }//GEN-LAST:event_jCheckBox3ItemStateChanged
 
     private void jCheckBox4ItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_jCheckBox4ItemStateChanged
@@ -280,12 +360,31 @@ public class NewJFrame extends javax.swing.JFrame {
         System.out.println("Opzione blu cambiata");
         int blue = evt.getStateChange();
         System.out.println("Stato: " + blue);
-        if(blue == ItemEvent.SELECTED){
+        if (blue == ItemEvent.SELECTED) {
             System.out.println("Colore blue selezionato!");
+            isBlueCheked = true;
+            if (isRedCheked && isBlueCheked && isGreenCheked) {
+                jCheckBox1.setEnabled(false);
+                jCheckBox1.setSelected(true);
+            }
+            jCheckBox3.setEnabled(true);
+            jCheckBox2.setEnabled(true);
         }
-        if(blue == ItemEvent.DESELECTED){
+        if (blue == ItemEvent.DESELECTED) {
+            jCheckBox1.setEnabled(true);
+            jCheckBox1.setSelected(false);
             System.out.println("Colore blue deselezionato");
+            if (isRedCheked || isGreenCheked) {
+                isBlueCheked = false;
+            }
+            if (isRedCheked && !isGreenCheked) {
+                jCheckBox2.setEnabled(false);
+            } else if (!isRedCheked && isGreenCheked) {
+                jCheckBox3.setEnabled(false);
+            }
         }
+        setColors();
+        setLocation();
     }//GEN-LAST:event_jCheckBox4ItemStateChanged
 
     private void jCheckBox1ItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_jCheckBox1ItemStateChanged
@@ -293,27 +392,44 @@ public class NewJFrame extends javax.swing.JFrame {
         System.out.println("Opzione tutti i colori cambiata");
         int all = evt.getStateChange();
         System.out.println("Stato: " + all);
-        if(all == ItemEvent.SELECTED){
+        if (all == ItemEvent.SELECTED) {
             System.out.println("Tutti i colori selezionati!");
+            isRedCheked = true;
+            isGreenCheked = true;
+            isBlueCheked = true;
+            jCheckBox2.setSelected(true);
+            jCheckBox3.setSelected(true);
+            jCheckBox4.setSelected(true);
+            jCheckBox2.setEnabled(true);
+            jCheckBox3.setEnabled(true);
+            jCheckBox4.setEnabled(true);
         }
-        if(all == ItemEvent.DESELECTED){
+        if (all == ItemEvent.DESELECTED) {
             System.out.println("Tutti i colori deselezionati");
         }
+        setColors();
     }//GEN-LAST:event_jCheckBox1ItemStateChanged
 
     private void jRadioButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton1ActionPerformed
         // TODO add your handling code here:
         System.out.println("Logo posizionato in alto a destra");
+        location = PictureLocation.RIGHT_UP;
+        setLocation();
+        setLocation();
     }//GEN-LAST:event_jRadioButton1ActionPerformed
 
     private void jRadioButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton2ActionPerformed
         // TODO add your handling code here:
         System.out.println("Logo posizionato in alto a sinistra");
+        location = PictureLocation.LEFT_UP;
+        setLocation();
     }//GEN-LAST:event_jRadioButton2ActionPerformed
 
     private void jRadioButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton3ActionPerformed
         // TODO add your handling code here:
         System.out.println("Logo posizionato in basso a destra");
+        location = PictureLocation.RIGHT_DOWN;
+        setLocation();
     }//GEN-LAST:event_jRadioButton3ActionPerformed
 
     /**
@@ -351,6 +467,143 @@ public class NewJFrame extends javax.swing.JFrame {
         });
     }
 
+    private void setColors() {
+        BufferedImage image = this.lienzo1.getImage();
+        int width = image.getWidth();
+        int height = image.getHeight();
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int rgb = image.getRGB(i, j);
+                Color color = new Color(rgb);
+                Color pointColor = colors.get(new Point(i, j));
+                int red = color.getRed();
+                int green = color.getGreen();
+                int blue = color.getBlue();
+                int alpha = color.getAlpha();
+                if (!isRedCheked) {
+                    red = 0;
+                } else {
+                    red = pointColor.getRed();
+                }
+                if (!isGreenCheked) {
+                    green = 0;
+                } else {
+                    green = pointColor.getRed();
+                }
+                if (!isBlueCheked) {
+                    blue = 0;
+                } else {
+                    blue = pointColor.getBlue();
+                }
+                int newRgb = new Color(red, green, blue, alpha).getRGB();
+                image.setRGB(i, j, newRgb);
+            }
+        }
+        
+      lienzo1.repaint();
+      setLocation();
+    }
+    
+    
+      private void setLocation() {
+        BufferedImage image = this.lienzo1.getImage();
+        deleteLogo(image);
+        BufferedImage image2 = this.lienzo1.getImage2();
+        switch (location) {
+            case RIGHT_UP:
+                paintRightUp(image, image2);
+                break;
+            case LEFT_UP:
+                paintLeftUp(image, image2);
+                break;
+            case RIGHT_DOWN:
+                paintRightDown(image, image2);
+                break;
+            case LEFT_DOWN:
+                paintLeftDown(image, image2);
+                break;
+            default:
+                break;
+        }
+        lienzo1.repaint();
+     }
+    
+    private void deleteLogo(BufferedImage image) {
+        for (Map.Entry<Point, Integer> entry  : pointsToDeleteLogo.entrySet()) {
+            Point key = entry.getKey();
+            Integer value = entry.getValue();           
+            image.setRGB((int)entry.getKey().getX(), (int)entry.getKey().getY(), value);
+        }
+        pointsToDeleteLogo.clear();
+    }  
+      
+    private void paintRightUp(BufferedImage image, BufferedImage image2) {
+        int height1 = image.getHeight();
+        int width1 = image.getWidth();
+        int height2 = image2.getHeight();
+        int width2 = image2.getWidth();
+        for (int i = width1 - width2; i < width1; i++ ) {
+            for (int j = 0; j < height2; j++) {
+                int rgb = image2.getRGB(i - width1 + width2, j);
+                pointsToDeleteLogo.put(new Point(i, j), image.getRGB(i, j));
+                image.setRGB(i, j, rgb);
+            }
+        }
+    }
+    
+    private void paintLeftUp(BufferedImage image, BufferedImage image2) {
+        int height2 = image2.getHeight();
+        int width2 = image2.getWidth();
+        for (int i = 0; i < width2; i++) {
+            for (int j = 0; j < height2; j++) {
+                int rgb = image2.getRGB(i, j);
+                pointsToDeleteLogo.put(new Point(i, j), image.getRGB(i, j));
+                image.setRGB(i, j, rgb);
+            }
+        }
+        
+    }
+    
+    private void paintRightDown(BufferedImage image, BufferedImage image2) {
+        int height1 = image.getHeight();
+        int width1 = image.getWidth();
+        int height2 = image2.getHeight();
+        int width2 = image2.getWidth();
+        for (int i = width1 - width2; i < width1; i++) {
+            for (int j = height1 - height2; j < height1; j++) {
+                int rgb = image2.getRGB(i - width1 + width2, j - height1 + height2);
+                pointsToDeleteLogo.put(new Point(i, j), image.getRGB(i, j));
+                image.setRGB(i, j, rgb);
+            }
+        }
+    }
+    
+    private void paintLeftDown(BufferedImage image, BufferedImage image2) {
+        int height1 = image.getHeight();
+        int width1 = image.getWidth();
+        int height2 = image2.getHeight();
+        int width2 = image2.getWidth();
+        for (int i = 0; i < width2; i++) {
+            for (int j = height1 - height2; j < height1; j++) {
+                int rgb = image2.getRGB(i, j - height1 + height2);
+                pointsToDeleteLogo.put(new Point(i, j), image.getRGB(i, j));
+                image.setRGB(i, j, rgb);
+            }
+        }
+    }
+
+    private void initializeColors() {
+        BufferedImage image = lienzo1.getImage();
+        for (int i = 0; i < image.getWidth(); i++) {
+            for (int j = 0; j < image.getHeight(); j++) {
+                int rgb = image.getRGB(i, j);
+                Color color = new Color(rgb);
+                colors.put(new Point(i, j), color);
+            }
+        }
+    }
+
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.JCheckBox jCheckBox1;
@@ -365,4 +618,11 @@ public class NewJFrame extends javax.swing.JFrame {
     private javax.swing.JRadioButton jRadioButton4;
     private com.mycompany.practicas5.Lienzo lienzo1;
     // End of variables declaration//GEN-END:variables
+enum PictureLocation {
+        LEFT_UP,
+        RIGHT_UP,
+        LEFT_DOWN,
+        RIGHT_DOWN
+    }
+
 }
